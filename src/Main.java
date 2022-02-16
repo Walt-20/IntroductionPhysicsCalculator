5b8f46ea8d9a1d8f6f470285c4ddbee4b2921ab3
Walt <walt.c0des20@gmail.com>
2022-02-16 14:51:45 -0700

    editing main menu

--- src/Main.java
+++ src/Main.java
@@ -10,35 +10,61 @@ import java.util.Scanner;
 
 public class Main {
 
+    boolean exit;
+
     public static void main(String[] args) {
-        // write your code here
-        /*
-         *
-         * declaration of variables
-         *
-         */
-        Scanner in = new Scanner(System.in);
-        int menuInput = in.nextInt();
 
-        /*
-         *
-         * Main Menu
-         *
-         */
-        while (menuInput != 0) {
-            System.out.println("Convert Units to ISU: 1");
-            /*
-             *
-             * Conversion, imperial units and metric to International System of Units
-             *
-             */
-            if (menuInput == 1) {
-                System.out.println("MPH  to Meters?");
-                double mphConvert = in.nextDouble();
-
-            }
-
-        }
+        Main menu = new Main();
+        menu.runner();
     }
+
+        public void runner() {
+            printHeader();
+            while (!exit) {
+                printHeader();
+                int userInput = getInput();
+                performAction(userInput);
+            }
+        }
+
+        private void printHeader() {
+            System.out.println("**************************************************");
+            System.out.println("|           Physics Calculator                   |");
+            System.out.println("|            Convert to ISU                      |");
+            System.out.println("**************************************************");
+        }
+
+        private void printMenu() {
+            System.out.println("0 - to quit menu");
+            System.out.println("1 - Convert to ISU");
+        }
+
+        private int getInput() {
+            Scanner scr = new Scanner(System.in);
+            int menuInput = -1;
+            while (menuInput < 0) {
+                try {
+                    menuInput = Integer.parseInt(scr.nextLine());
+                }
+                catch (NumberFormatException e) {
+                    System.out.println("Your menu options are");
+                }
+            }
+            return menuInput;
+        }
+
+        private void performAction(int menuInput) {
+            switch (menuInput) {
+                case 0:
+                    exit = true;
+                    System.out.println("Closed Application");
+                    break;
+                case 1:
+                    convertUnits();
+                    break;
+                default:
+                    System.out.println("An unknown error has accured");
+            }
+        }
 
 }
